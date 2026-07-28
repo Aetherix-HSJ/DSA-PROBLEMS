06052be01bfae1606c2729945684872de1d65399
Himanshu Shekhar Jha <jhahimanshu458@gmail.com>
2026-07-28 15:52:11 +0530

    Added solution - Time: O(N), Space: O(1)

---
+++ 713150-first-node-of-loop-in-linked-list-GFG/713150-first-node-of-loop-in-linked-list.java
@@ -0,0 +1,37 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int x)
+    {
+        data = x;
+        next = null;
+    }
+};
+*/
+
+class Solution {
+    public int cycleStart(Node head) {
+        // code here
+        Node slow = head;
+        Node fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast) break;
+        }
+        if(slow!=fast) return -1;
+        Node b = head;
+        while(b!=slow){
+            b = b.next;
+            slow = slow.next;
+        }
+        if(b==slow) return slow.data;
+        return -1;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
