7a60cf0c56cabba3b06e48e1c9ebe495d3a9f6bf
Himanshu Shekhar Jha <jhahimanshu458@gmail.com>
2026-08-28 04:44:19 +0530

    Time: 43 ms (36.2%), Space: 46.4 MB (22.73%) - LeetHub - Time: O(n * k), Space: O(n)

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,16 +1,17 @@
+import java.util.*;
 class Solution {
     public int findTheWinner(int n, int k) {
         Queue<Integer> q = new LinkedList<>();
         for(int i=1; i<=n; i++){
             q.add(i);
         }
-        while(q.size()!=1){
+        while(q.size()>1){
             for(int i=0; i<k-1; i++){
                 q.add(q.remove());
             }
-            q.remove();
+            q.remove(q.peek());
         }
-        return q.remove();
+        return q.peek();
     }
 }
 
