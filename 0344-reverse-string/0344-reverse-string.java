51257c6c91af59248c008e391994adb542fcb088
Himanshu Shekhar Jha <jhahimanshu458@gmail.com>
2026-09-02 22:40:39 +0530

    Time: 0 ms (100%), Space: 48.1 MB (74.2%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] arr) {
+        int i = 0; int j = arr.length-1;
+        while(i<j){
+            char temp= arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
