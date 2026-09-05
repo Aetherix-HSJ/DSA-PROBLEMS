151621ad0cd5ce2d394ff5be7fbe545629c2f097
Himanshu Shekhar Jha <jhahimanshu458@gmail.com>
2026-09-05 22:43:35 +0530

    Time: 7 ms (65.23%), Space: 47.1 MB (54.31%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i = 0, j = 0;
+        while(i!=s.length() && j!=t.length()){
+            if(s.charAt(i)==t.charAt(j)) {
+                i++;
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return t.length()-j;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
