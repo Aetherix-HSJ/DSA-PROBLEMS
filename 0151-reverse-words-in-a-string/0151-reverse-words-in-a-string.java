dead54cc57f3bc6b0ca425fc90651a03f36f7da6
Himanshu Shekhar Jha <jhahimanshu458@gmail.com>
2026-09-06 05:26:09 +0530

    Time: 7 ms (63.24%), Space: 43.9 MB (94.77%) - LeetHub - Time: O(n), Space: O(n)

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,16 +1,49 @@
 class Solution {
-    public String reverseWords(String s) {
-        s = s.trim();
-        String[] words = s.split("\\s+");
-        int i = 0, j = words.length-1;
-        while(i<j){
-            String temp = words[i];
-            words[i] = words[j];
-            words[j] = temp;
+    static void reverse(int i, int j, StringBuilder SB){
+        while(i<=j){
+            char temp = SB.charAt(i);
+            SB.setCharAt(i,SB.charAt(j));
+            SB.setCharAt(j,temp);
             i++;
             j--;
         }
-        return String.join(" ",words);
+    }
+    public String reverseWords(String s) {
+        int l = 0, r = s.length()-1;
+        while(l<s.length()){
+            if(s.charAt(l)==' ') l++;
+            else break;
+        }
+        while(r>=0){
+            if(s.charAt(r)==' ') r--;
+            else break;
+        }
+
+        StringBuilder SB = new StringBuilder();
+        while(l<=r){
+              if(s.charAt(l)!=' '){
+                SB.append(s.charAt(l));
+                l++;
+              }
+              else if(s.charAt(l)==' '){
+                if(s.charAt(l-1)!=' ') SB.append(s.charAt(l));
+                l++;
+              }
+        }
+        // reverse StringBuilder
+        int i = 0, j = SB.length()-1;
+        reverse(i,j,SB);
+        //reverse words
+        i = 0; j=0;
+        while(i<SB.length()){
+            while(j<SB.length() && SB.charAt(j)!=' ') {
+                j++;
+            }
+            reverse(i,j-1,SB);
+                i = j+1;
+                j = i;
+        }
+        return SB.toString();
     }
 }
 
