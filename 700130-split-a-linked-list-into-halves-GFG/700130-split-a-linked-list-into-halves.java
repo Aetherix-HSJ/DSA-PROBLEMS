8a6a73ecc335cacdb85f999499a638123775d7bf
Himanshu Shekhar Jha <jhahimanshu458@gmail.com>
2026-09-10 23:14:01 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 700130-split-a-linked-list-into-halves-GFG/700130-split-a-linked-list-into-halves.java
@@ -0,0 +1,26 @@
+class Solution {
+    public Pair<Node, Node> splitList(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != head && fast.next != head) {
+            slow = slow.next;
+            fast = fast.next;
+
+            if (fast.next!= head) {
+                fast = fast.next;
+            }
+        }
+
+        Node head2 = slow.next;
+
+        fast.next = head2;
+        slow.next = head;
+
+        return new Pair<>(head, head2);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
